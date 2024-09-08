80f099473de718c0249b8bc52029dd8a532bbbd6
Tahsin <alahitahsin@gmail.com>
2024-09-08 22:37:13 +0600

    added Homepage Narbar

---
+++ src/pages/AppLayout/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppNav from "../../components/AppNav/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>AppLayout</h1>
+    </div>
+  );
+}
+
+export default AppLayout;

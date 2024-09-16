e33a29d5fbcac351fe87e7b7ed60bc6f4a5b1abd
Tahsin <alahitahsin@gmail.com>
2024-09-16 16:26:07 +0600

    added deleting function

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -56,9 +56,33 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, { method: "DELETE" });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+      // await fetch(`${BASE_URL}/cities`, {
+      //   method: "POST",
+      //   body: JSON.stringify(cities),
+      //   headers: { "Content-Type": "application/json" },
+      // });
+    } catch {
+      console.log("There were an error deleting the city.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, getCurrentCity, currentCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        getCurrentCity,
+        currentCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
